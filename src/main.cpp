ee63f7467c61e6fa865deb2c8a2d4a8531dc0073
kruziikrel13 <dev@michaelpetersen.io>
2025-09-15 11:22:51 +1000

    feat: add example library linking and usage to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <spdlog/spdlog.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    spdlog::info("This is an informative log message from spdlog.");
     return 0;
 }
